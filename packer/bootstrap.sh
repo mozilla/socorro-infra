d8aa4ff5c85812afaf5e2d8f7b10795c5c56b89a
phrawzty <phrawzty+github@gmail.com>
2015-01-12 18:01:33 +0100

    add packer default build

---
+++ packer/bootstrap.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+function techo {
+    STAMP=`date '+%b %d %H:%M:%S'`
+    echo "${STAMP} BOOTSTRAP: ${@}"
+}
+
+techo "start"
+techo "install puppet yum repo"
+rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
+techo "yum check-update"
+yum check-update
+techo "yum upgrade"
+yum -y upgrade
+techo "yum install puppet"
+yum -y install puppet
+techo "end"
