46f399826dfd92dd6c07f547812ec0b091365b04
phrawzty <phrawzty+github@gmail.com>
2015-02-09 17:04:09 +0100

    use us-west-2 by default

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,19 +3,22 @@ variable "secret_key" {}
 variable "ssh_key_file" {
     default = {
         eu-west-1 = "socorro__eu-west-1.pem"
+        us-west-2 = "socorro__us-west-2.pem"
     }
 }
 variable "region" {
-    default = "eu-west-1"
+    default = "us-west-2"
 }
 variable "ssh_key_name" {
     default = {
         eu-west-1 = "socorro__eu-west-1"
+        us-west-2 = "socorro__us-west-2"
     }
 }
 variable "base_ami" {
     default = {
         eu-west-1 = "ami-332fa744"
+        us-west-2 = "ami-1d2e0b2d"
     }
 }
 variable "alt_ssh_port" {
