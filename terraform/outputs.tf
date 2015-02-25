476ce94123bbc73c7e3febd1d183d933b06be3be
Robert Helmer <rhelmer@rhelmer.org>
2015-02-25 11:39:32 -0800

    put an ELB in front of symbolapi, so we can have somewhere to point DNS to (and also for autoscaling potentially, later)

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
+output "public_addr__symbolapi__http" {
+    value = "${aws_elb.elb_for_symbolapi.dns_name}"
+}
+
 output "public_addr__webhead__http" {
     value = "${aws_elb.elb_for_webhead.dns_name}"
 }
