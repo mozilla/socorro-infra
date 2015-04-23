2a4a1576b41eb1450ba978d2336cc6d14ff4c767
Robert Helmer <rhelmer@rhelmer.org>
2015-04-23 16:50:13 -0700

    fix tests

---
+++ bin/test.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+TFORM_VERSION="0.4.2_linux_amd64"
+
+gem install puppet puppet-lint
+puppet-lint --with-filename --no-80chars-check --no-autoloader_layout-check --fail-on-warnings puppet/
+puppet parser validate `find puppet/ -name '*.pp'`
+
+pushd terraform
+wget "https://dl.bintray.com/mitchellh/terraform/terraform_${TFORM_VERSION}.zip"
+unzip -u terraform_${TFORM_VERSION}.zip
+./wrapper.sh symlinks
+popd
+
+for role in $(find ./terraform/* -maxdepth 1 -type d); do
+    pushd $role
+    ../terraform plan -var="environment=FAKE" \
+                   -var="secret_key=FAKE" \
+                   -var="access_key=FAKE" \
+                   -var="subnets=FAKE" \
+                   -var="secret_bucket=FAKE" \
+                   -var="collector_cert=FAKE" \
+                   -var="analysis_cert=FAKE" \
+                   -var="webapp_cert=FAKE"
+    popd
+done
