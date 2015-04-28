75a2598a778e5f40585c6ee5be099c160f119744
phrawzty <phrawzty+github@gmail.com>
2015-04-28 17:50:48 +0200

    nobug; amplify docs and minor fixes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -38,7 +38,3 @@ variable "alt_ssh_port" {
 variable "puppet_archive" {
     default = "https://s3-us-west-2.amazonaws.com/org.mozilla.crash-stats.packages-public/prov_cache/socorro-infra__puppet.tar.gz"
 }
-# NOTE - this deletes EBS devices, only change it for testing purposes!
-variable "del_on_term" {
-    default = "false"
-}
