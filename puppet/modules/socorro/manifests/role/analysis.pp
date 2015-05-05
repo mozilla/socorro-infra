3323b0580009a6a04144f2b866cd62ea4a360e93
Robert Helmer <rhelmer@rhelmer.org>
2015-05-05 12:01:24 -0700

    bug 1152621 - activate nginx configs when nodes are provisioned by ASG

--- puppet/modules/socorro/manifests/role/analysis.pp
+++ puppet/modules/socorro/manifests/role/analysis.pp
@@ -7,7 +7,25 @@ include socorro::role::common
     'nginx':
       ensure  => running,
       enable  => true,
-      require => Package['nginx']
+      require => Package['nginx'],
+      subscribe => File['/etc/nginx/conf.d/socorro-analysis.conf'];
+  }
+
+  file {
+    '/etc/nginx/nginx.conf':
+      source  => 'puppet:///modules/socorro/etc_nginx/nginx.conf',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0664',
+      require => Package['nginx'];
+
+    '/etc/nginx/conf.d/socorro-analysis.conf':
+      source  =>
+        'puppet:///modules/socorro/etc_nginx/conf_d/socorro-analysis.conf',
+      owner   => 'root',
+      group   => 'nginx',
+      mode    => '0664',
+      require => File['/etc/nginx/nginx.conf'];
   }
 
   package {
