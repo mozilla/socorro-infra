c166be719bb7ae48fe8eb83aaf3cdf7e8d1a16ba
Robert Helmer <rhelmer@rhelmer.org>
2015-05-08 12:08:22 -0700

    use variables instead of magic position numbers

--- terraform/socorro_role.sh
+++ terraform/socorro_role.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
+if [ $# != 3 ]; then
+    echo "Syntax: $0 <role> <secret_bucket> <env>"
+    exit 1
+fi
+
+ROLE=$1
+SECRET_BUCKET=$2
+ENV=$3
+
 function socorro_role {
     DIR="/etc/puppet"
 
     # Provide the secret bucket name to Hiera (hiera-s3).
-    sed -i "s:@@@SECRET_BUCKET@@@:${3}:" /etc/puppet/hiera.yaml
+    sed -i "s:@@@SECRET_BUCKET@@@:${SECRET_BUCKET}:" /etc/puppet/hiera.yaml
 
     # Provision the role.
-    /usr/bin/env FACTER_socorro_role=$1 FACTER_environment=$3 \
+    /usr/bin/env FACTER_socorro_role=$ROLE FACTER_environment=$ENV \
         puppet apply \
         --modulepath=${DIR}/module-0:/etc/puppet/modules \
         ${DIR}/manifests/default.pp
