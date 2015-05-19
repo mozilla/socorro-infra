71da299875a7f00a97d46c25d855a80328d9f0a6
Robert Helmer <rhelmer@rhelmer.org>
2015-05-19 13:22:01 -0700

    bug 1146447 - re-join the consul cluster when restarting in server mode

--- puppet/modules/socorro/manifests/role/consul.pp
+++ puppet/modules/socorro/manifests/role/consul.pp
@@ -18,4 +18,12 @@ include socorro::role::common
       mode   => '0640';
   }
 
+  # We expect this to come from the secret S3 bucket
+  $consul_hostname = hiera("${::environment}/consul_hostname")
+  exec {
+    'join_consul_server_cluster':
+      command  => "/usr/bin/consul join ${consul_hostname}",
+      requires => Service['consul'];
+  }
+
 }
