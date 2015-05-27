1d0c37c406640df217cdafdec804362e72c43ad1
phrawzty <phrawzty+github@gmail.com>
2015-05-27 14:34:32 +0200

    format and mount EBS for data nodes

--- puppet/modules/socorro/manifests/role/elasticsearch.pp
+++ puppet/modules/socorro/manifests/role/elasticsearch.pp
@@ -4,11 +4,12 @@ class socorro::role::elasticsearch {
 include socorro::role::common
 
   # ES will hit default ulimits rather quickly.
-  file { '/etc/security/limits.d/90-elasticsearch.conf':
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    source => 'puppet:///modules/socorro/etc_security_limits.d/90-elasticsearch.conf'
+  file {
+    '/etc/security/limits.d/90-elasticsearch.conf':
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
+      source => 'puppet:///modules/socorro/etc_security_limits.d/90-elasticsearch.conf'
   }
 
   # These switches determine the role of the node: master, interface, or data.
@@ -26,11 +27,17 @@ include socorro::role::common
   }
 
   # The values from the switches above are applied in elasticsearch.yml .
-  file { '/etc/elasticsearch/elasticsearch.yml':
-    owner   => 'root',
-    group   => 'elasticsearch',
-    mode    => '0644',
-    content => template('socorro/etc_elasticsearch/elasticsearch.yml.erb')
+  file {
+    '/etc/elasticsearch/elasticsearch.yml':
+      owner   => 'root',
+      group   => 'elasticsearch',
+      mode    => '0644',
+      content => template('socorro/etc_elasticsearch/elasticsearch.yml.erb')
+  }
+
+  file {
+    '/var/lib/elasticsearch':
+      owner => 'elasticsearch'
   }
 
   service {
@@ -40,8 +47,32 @@ include socorro::role::common
       require => [
         Exec['join_consul_cluster'],
         File['/etc/security/limits.d/90-elasticsearch.conf'],
-        File['/etc/elasticsearch/elasticsearch.yml']
+        File['/etc/elasticsearch/elasticsearch.yml'],
+        File['/var/lib/elasticsearch']
       ]
+  }
+
+  # Data nodes have an attached EBS volume that needs to be formatted and
+  # mounted before the service is activated.
+  if $::elasticsearch_role == 'data' {
+    exec {
+      'format-es-data':
+        path    => '/usr/sbin',
+        command => 'mkfs.ext4 /dev/xvdb'
+    }
+
+    mount {
+      '/var/lib/elasticsearch':
+        ensure  => mounted,
+        device  => '/dev/xvdb',
+        fstype  => 'ext4',
+        options => 'defaults',
+        require => Exec['format-es-data']
+    }
+
+    File['/var/lib/elasticsearch'] {
+      require => Mount['/var/lib/elasticsearch']
+    }
   }
 
 }
