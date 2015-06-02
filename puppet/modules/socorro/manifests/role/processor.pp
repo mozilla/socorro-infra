57fd741e495d36b0c82b8184ae9becad2c058897
Robert Helmer <rhelmer@rhelmer.org>
2015-06-01 23:04:15 -0700

    workaround bug 1170420 - ensure symbols cache dir exists, until this is fixed in Socorro Processor

--- puppet/modules/socorro/manifests/role/processor.pp
+++ puppet/modules/socorro/manifests/role/processor.pp
@@ -3,11 +3,21 @@ class socorro::role::processor {
 
 include socorro::role::common
 
+  # FIXME - remove this when bug 1170420 is fixed in Socorro Processor
+  file {
+    '/tmp/symbols':
+      ensure => 'directory',
+      owner  => socorro
+  }
+
   service {
     'socorro-processor':
       ensure  => running,
       enable  => true,
-      require => Exec['join_consul_cluster'];
+      require => [
+        Exec['join_consul_cluster'],
+        File['/tmp/symbols']
+      ];
   }
 
 }
