4ca8a239807ab90960afa6fa3b406e588aae793a
phrawzty <phrawzty+github@gmail.com>
2015-06-11 17:50:52 +0200

    Alt LB for Collectors that uses an old, dangerous SSL config

--- terraform/collector/main.tf
+++ terraform/collector/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 resource "aws_security_group" "ec2-collector-sg" {
     name = "ec2-collector-${var.environment}-sg"
-    description = "Security grup for ec2 as group for socorro collector."
+    description = "Security group for EC2 socorro collector."
     ingress {
         from_port = "${var.alt_ssh_port}"
         to_port = "${var.alt_ssh_port}"
@@ -90,11 +90,11 @@ resource "aws_elb" "elb-collector" {
         "${var.elb_master_web_sg_id}"
     ]
     health_check {
-      healthy_threshold = 2
-      unhealthy_threshold = 2
-      timeout = 3
-      target = "TCP:80"
-      interval = 12
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+        timeout = 3
+        target = "TCP:80"
+        interval = 12
     }
     tags {
         Environment = "${var.environment}"
@@ -104,6 +104,53 @@ resource "aws_elb" "elb-collector" {
     cross_zone_load_balancing = true
     connection_draining = true
     connection_draining_timeout = 30
+}
+
+resource "aws_elb" "elb-collector-oldssl" {
+    name = "elb-${var.environment}-collector-oldssl"
+    availability_zones = [
+        "${var.region}a",
+        "${var.region}b",
+        "${var.region}c"
+    ]
+    listener {
+        instance_port = 80
+        instance_protocol = "http"
+        lb_port = 80
+        lb_protocol = "http"
+    }
+    listener {
+        instance_port = 80
+        instance_protocol = "http"
+        lb_port = 443
+        lb_protocol = "https"
+        ssl_certificate_id = "${var.collector_cert}"
+    }
+    security_groups = [
+        "${var.elb_master_web_sg_id}"
+    ]
+    health_check {
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+        timeout = 3
+        target = "TCP:80"
+        interval = 12
+    }
+    tags {
+        Environment = "${var.environment}"
+        role = "collector"
+        project = "socorro"
+    }
+    cross_zone_load_balancing = true
+    connection_draining = true
+    connection_draining_timeout = 30
+    ### SSL policies aren't handled natively by TF, so here we go...
+    provisioner "local-exec" {
+        command = "aws elb create-load-balancer-policy --region ${var.region} --load-balancer-name ${aws_elb.elb-collector-oldssl.name} --policy-name oldssl --policy-type-name SSLNegotiationPolicyType --policy-attributes AttributeName=Reference-Security-Policy,AttributeValue=ELBSecurityPolicy-2011-08"
+    }
+    provisioner "local-exec" {
+        command = "aws elb set-load-balancer-policies-of-listener --region ${var.region} --load-balancer-name ${aws_elb.elb-collector-oldssl.name} --load-balancer-port 443 --policy-names oldssl"
+    }
 }
 
 resource "aws_launch_configuration" "lc-collector" {
@@ -137,21 +184,22 @@ resource "aws_autoscaling_group" "as-collector" {
     min_size = "${lookup(var.collector_num, var.environment)}"
     desired_capacity = "${lookup(var.collector_num, var.environment)}"
     load_balancers = [
-        "elb-${var.environment}-collector"
+        "elb-${var.environment}-collector",
+        "elb-${var.environment}-collector-oldssl"
     ]
     tag {
-      key = "Environment"
-      value = "${var.environment}"
-      propagate_at_launch = true
+        key = "Environment"
+        value = "${var.environment}"
+        propagate_at_launch = true
     }
     tag {
-      key = "role"
-      value = "collector"
-      propagate_at_launch = true
+        key = "role"
+        value = "collector"
+        propagate_at_launch = true
     }
     tag {
-      key = "project"
-      value = "socorro"
-      propagate_at_launch = true
+        key = "project"
+        value = "socorro"
+        propagate_at_launch = true
     }
 }
--- terraform/collector/outputs.tf
+++ terraform/collector/outputs.tf
@@ -1,3 +1,7 @@
 output "public_addr__collector__http" {
     value = "${aws_elb.elb-collector.dns_name}"
 }
+
+output "public_addr__collector__oldssl" {
+    value = "${aws_elb.elb-collector-oldssl.dns_name}"
+}
