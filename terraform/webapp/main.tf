7a69d3c2fba7388807ccc0cb8256e928f08aa28b
JP Schneider <johns@mozillafoundation.org>
2016-01-15 12:31:17 -0600

    Change the health check to use http looking at a json file to ensure we don't exercise the db and cache so heavily

--- terraform/webapp/main.tf
+++ terraform/webapp/main.tf
@@ -144,7 +144,7 @@ resource "aws_elb" "elb-socorroweb" {
       healthy_threshold = 2
       unhealthy_threshold = 2
       timeout = 3
-      target = "TCP:80"
+      target = "HTTP:80/contribute.json"
       interval = 12
     }
     security_groups = [
